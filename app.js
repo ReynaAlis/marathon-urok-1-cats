dd38a1a687a041950981d8066701c26dfa581d4e
ReynaAlis <alaingaranzha@gmail.com>
2023-07-14 21:00:16 +0300

    bonus js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active')
-})
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-slides.forEach((slide) => {
-    slide.classList.remove('active')
-})
-}
+slidesPlugin()
+// в скобках можно задать номер слайда, который будет активным (от 0 до 4). если ничего не указать, то активным будет слайд № 0 (см. первую строку)
